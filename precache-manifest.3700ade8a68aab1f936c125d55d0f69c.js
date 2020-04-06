573fae2248a3954917bb908c490cd11206d61bb7
Alptuğ Türkiş <alptug.turkis@calisma.local>
2020-04-06 14:52:21 +0300

    initial commit

---
+++ precache-manifest.3700ade8a68aab1f936c125d55d0f69c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666ed94bb74b9ad862f8bf4239769938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e67c4b709550ffaa99c2",
+    "url": "/static/css/2.a509cf1f.chunk.css"
+  },
+  {
+    "revision": "a5cdad6f3ccafb0f630f",
+    "url": "/static/css/3.c99205e1.chunk.css"
+  },
+  {
+    "revision": "ae00d061b24ba6ca18ef",
+    "url": "/static/css/4.c99205e1.chunk.css"
+  },
+  {
+    "revision": "483e8cfe34ebcdbe3340",
+    "url": "/static/css/5.c99205e1.chunk.css"
+  },
+  {
+    "revision": "5d536f1fd1abd3e494ce",
+    "url": "/static/css/main.27ff35e5.chunk.css"
+  },
+  {
+    "revision": "e67c4b709550ffaa99c2",
+    "url": "/static/js/2.245ab6bf.chunk.js"
+  },
+  {
+    "revision": "0871d0884e554e3f65a1da48bb2aa6fd",
+    "url": "/static/js/2.245ab6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5cdad6f3ccafb0f630f",
+    "url": "/static/js/3.af9b900b.chunk.js"
+  },
+  {
+    "revision": "ae00d061b24ba6ca18ef",
+    "url": "/static/js/4.32131072.chunk.js"
+  },
+  {
+    "revision": "483e8cfe34ebcdbe3340",
+    "url": "/static/js/5.0933fb0b.chunk.js"
+  },
+  {
+    "revision": "5d536f1fd1abd3e494ce",
+    "url": "/static/js/main.736526f8.chunk.js"
+  },
+  {
+    "revision": "3a71127c45e00bcc6fbc",
+    "url": "/static/js/runtime-main.c8e6139b.js"
+  },
+  {
+    "revision": "d228921bb51471e4fd52d8c3654d2e84",
+    "url": "/static/media/armali-logo-tr.d228921b.svg"
+  },
+  {
+    "revision": "392c998bdb0f0a8197c2acf448923caf",
+    "url": "/static/media/icomoon.392c998b.woff"
+  },
+  {
+    "revision": "c7d6732018721226338b160dc602ab44",
+    "url": "/static/media/icomoon.c7d67320.eot"
+  },
+  {
+    "revision": "ed3261fa9a0b710566935062fe48a207",
+    "url": "/static/media/icomoon.ed3261fa.ttf"
+  },
+  {
+    "revision": "f87ba56d0ec780c7aebbb404775f099b",
+    "url": "/static/media/icomoon.f87ba56d.svg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
