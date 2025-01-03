d5e8226812218f58776279332d8053fe34aa0b2f
Alptuğ Türkiş <alptug.turkis@outlook.com>
2025-01-04 00:09:26 +0300

    Add canvas element and implement basic drawing functionality

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,8 @@
-(function() {
-    console.log('Hello, World!');
-})()
+(function () {
+  window.onload = () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(50, 50, 100, 100);
+  };
+})();
