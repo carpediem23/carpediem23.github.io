9124f777301ad22409196a9b423dd403a16afd01
Alptuğ Türkiş <alptug.turkis@outlook.com>
2025-02-07 23:29:47 +0300

    Refactor service worker to dynamically cache URLs and handle cache deletion on activation

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
 const CACHE_NAME = "carpediem23-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles/global.css",
-  "./scripts/index.js",
-  "./assets/images/profile_transparent.webp",
-  "./assets/audios/ambience.mp3",
-  "./assets/audios/select.mp3",
-];
+
+let urlsToCache = [];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -20,10 +13,28 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches
       .match(event.request)
       .then((response) => response || fetch(event.request)),
   );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data.type === "CACHE_URLS") {
+    urlsToCache = event.data.urls;
+  }
 });
